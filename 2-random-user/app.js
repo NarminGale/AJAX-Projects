df31bff41e747f2f304564da86af930b9a70a552
NarminGale <narmingale@gmail.com>
2021-06-03 16:13:44 +0400

    Completed whole project

--- 2-random-user/app.js
+++ 2-random-user/app.js
@@ -1,17 +1,15 @@
 import get from './utils/getElement.js'
 import getUser from './utils/fetchUser.js'
+import displayUser from './utils/displayUser.js'
 
-const img = get('.user-img')
-const ttile = get('.user-title')
-const value = get('.user-value')
 const btn = get('.btn')
-// transforming nodelist to proper array
-const btns = [...document.querySelectorAll('.icon')]
 
 const showUser = async () => {
   // get user from api
   const person = await getUser()
-  console.log(person)
+  //   display user
+  displayUser(person)
 }
+
 window.addEventListener('DOMContentLoaded', showUser)
 btn.addEventListener('click', showUser)
