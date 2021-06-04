a73b1fcfcdec9045746a12a717e1ad322d9a77d7
NarminGale <narmingale@gmail.com>
2021-06-04 18:40:31 +0400

    Imported fetchDrinks.js

---
+++ 3-cocktails/src/presentDrinks.js
@@ -0,0 +1,10 @@
+import fetchDrinks from './fetchDrinks.js'
+
+const showDrinks = async (url) => {
+  // fetch drinks
+  const data = await fetchDrinks(url)
+  console.log(data)
+  // display drinks
+}
+
+export default showDrinks
